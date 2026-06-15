fa4cce17d2f193929ceabbde3e68d92080b772bc
klaus4772 <klaus@luga.de>
2026-06-15 18:03:57 +0200

    Remove generated build artifacts and update .gitignore

--- gaeb-schema-da85/target/generated-sources/xjc/com/example/gaebviewer/schema/da85/package-info.java
+++
@@ -1,8 +0,0 @@
-//
-// Diese Datei wurde mit der Eclipse Implementation of JAXB, v4.0.5 generiert 
-// Siehe https://eclipse-ee4j.github.io/jaxb-ri 
-// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-//
-
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://www.gaeb.de/GAEB_DA_XML/DA85/3.3", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.example.gaebviewer.schema.da85;
